a60fad3ed4e35354c5c42dd1e46c531165fcebf4
ych <yaochuhong1989@gmail.com>
2026-08-25 22:55:57 +0800

    build: prepare pagenest 1.9.260825

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.air5005.pagenest"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8.260825"
+        versionCode = 10
+        versionName = "1.9.260825"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
